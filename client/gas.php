27e732b70857b6ea705fc14fa4cccd38b705bd0a
CurlyMoo <CurlyMoo@users.noreply.github.com>
2013-08-12 20:03:27 +0200

    Update gas.php

--- client/gas.php
+++ client/gas.php
@@ -7,41 +7,32 @@ $rDatabase = mysql_select_db('*database*');
 $rDayGas = mysql_query("SELECT * FROM gas") or die(mysql_error());
 /* Make sure all missing hours are shows with zero's */
 $iNrRows = mysql_num_rows($rDayGas);
-$aGas = Array();
+$aPrevGas = Array();
+$sJson = '[';
 while($aDayGas = mysql_fetch_assoc($rDayGas)) {
-	$aGas[] = $aDayGas;
-	if(count($aGas) > 1) {
+	if(count($aPrevGas) > 1) {
 		$iDay = 0;
 		$bDaySet = false;
-		$iDatePrev = $aGas[count($aGas)-2]['datetime']/3600;
+		$iDatePrev = $aPrevGas['datetime']/3600;
 		$iDateCur = $aDayGas['datetime']/3600;
-		$iHourPrev = $aGas[count($aGas)-2]['hour'];
+		$iHourPrev = $aPrevGas['hour'];
 		
 		$iMissingHours = $iDateCur-$iDatePrev;
-		$iHourNew = $iHourPrev;
+		$iHourNew = $iHourPrev;		
+		
 		if($iMissingHours > 1) {
 			for($i=0;$i<$iMissingHours-1;$i++) {
-				$x = count($aGas)-1;
-				$aOldGas = $aGas[$x];
 				if((++$iHourNew) >= 24) {
 					$iHourPrev -= ($iHourNew-1);
 					$iHourNew = 0;
 					$iDay++;
 				}
-				$aGas[$x]['hour'] = $iHourNew;
-				$aGas[$x]['datetime'] = ($iDatePrev*3600)+($iDay*3600)+(($iHourNew-$iHourPrev)*3600);
-				$aGas[$x]['m3'] = 0;
-				$aGas[$x+1]=Array();
-				$aGas[$x+1]=$aOldGas;
+				$sJson .= '['.(($iDatePrev*3600)+($iDay*3600)+(($iHourNew-$iHourPrev)*3600)).'000,0],';
 			}
 		}
+		$sJson .= '['.$aDayGas['datetime'].'000,'.$aDayGas['m3'].'],';		
 	}
-}
-$sJson = '[';
-$i=0;
-foreach($aGas as $aDayGas) {
-	$sJson .= '['.$aDayGas['datetime'].'000,'.$aDayGas['m3'].'],';
-	$i++;
+	$aPrevGas = $aDayGas;
 }
 $sJson = substr($sJson, 0, -1);
 echo $sJson .= ']';
