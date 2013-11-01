e32fca3022103cf6da774aa20cd5410fdfa90abb
CurlyMoo <CurlyMoo@users.noreply.github.com>
2013-11-01 15:14:42 +0100

    Update electricity.php

--- client/electricity.php
+++ client/electricity.php
@@ -4,19 +4,23 @@ $rConnect = mysql_connect('x.x.x.x', '*username*', '*password*');
 $rDatabase = mysql_select_db('*database*');
 
 $rHourElec = mysql_query("SELECT * FROM electricity");
+$rTS = mysql_query("SELECT TIMEDIFF(NOW(), UTC_TIMESTAMP) as ts");
+$aTS = mysql_fetch_assoc($rTS);
 
-$iNrRows = mysql_num_rows($rHourElec);
 $sJson = '[';
 while($aHourElec = mysql_fetch_assoc($rHourElec)) {
-	$aDate = new DateTime(date("Y-m-d H:i:s", $aHourElec['datetime']-(2*3600)));
+
+	$aDate = new DateTime(date("Y-m-d H:i:s", $aHourElec['datetime']-(($aTS['ts']+1)*3600)), new DateTimeZone(date_default_timezone_get()));
 	if($aDate->format('I') == 0) {
-                $aHourElec['hour'] -= 1;
-                $aHourElec['datetime'] -= 3600;
-        }
-	if($aHourElec['hour'] >= 23 || $aHourElec['hour'] < 7)
+		$aHourElec['hour'] -= $aTS['ts'];
+		$aHourElec['datetime'] -= $aTS['ts']*3600;
+	}
+	if($aHourElec['hour'] >= 23 || $aHourElec['hour'] < 7) {
 		$sJson .= '{"x": '.$aHourElec['datetime'].'000, "y": '.$aHourElec['watt'].', "color": "#2f7ed8"},';
-	else
+	} else {
 		$sJson .= '{"x": '.$aHourElec['datetime'].'000, "y": '.$aHourElec['watt'].', "color": "#ffc600"},';
+	}
+	$sJson .= '['.$aHourElec['datetime'].'000,'.$aHourElec['watt'].'],';
 }
 $sJson = substr($sJson, 0, -1);
 echo $sJson .= ']';
