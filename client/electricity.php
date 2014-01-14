15f194e0c0969c4172685f3e3a7517d92ba1f45e
CurlyMoo <CurlyMoo@users.noreply.github.com>
2014-01-14 21:36:47 +0100

    Automatically remove error measurements

--- client/electricity.php
+++ client/electricity.php
@@ -4,13 +4,29 @@ $rConnect = mysql_connect('x.x.x.x', '*username*', '*password*');
 $rDatabase = mysql_select_db('*database*');
 
 $rHourElec = mysql_query("SELECT * FROM electricity");
+$rSE = mysql_query("SELECT MAX(`watt`) as `max`, AVG(`watt`) as `avg` FROM electricity");
 $rTS = mysql_query("SELECT TIMEDIFF(NOW(), UTC_TIMESTAMP) as ts");
 $aTS = mysql_fetch_assoc($rTS);
+$aSE = mysql_fetch_assoc($rSE);
 
+$iMax = ($aSE['avg'])*1.25;
 $sJson = '[';
 while($aHourElec = mysql_fetch_assoc($rHourElec)) {
 
 	$aDate = new DateTime(date("Y-m-d H:i:s", $aHourElec['datetime']-(($aTS['ts']+1)*3600)), new DateTimeZone(date_default_timezone_get()));
+	if($aHourElec['watt']/$aSE['avg'] > $iMax) {
+		$rRow = mysql_query("SELECT * FROM `electricity` WHERE `datetime` > '".($aHourElec['datetime']-1000)."' AND `datetime` < '".($aHourElec['datetime']+1000)."'") or die(mysql_error());
+		$iMean = 0;
+		$i = 0;
+		while($aRow = mysql_fetch_assoc($rRow)) {
+			if($aRow['watt'] < $iMax) {
+				$i++;
+				$iMean = $aRow['watt'];
+			}
+		}
+		$aHourElec['watt'] = $iMean;
+		mysql_query('UPDATE `electricity` SET `watt` = '.$iMean.' WHERE `datetime` = '.$aHourElec['datetime']) or die(mysql_error());
+	}
 	if($aDate->format('I') == 0) {
 		$aHourElec['datetime'] -= $aTS['ts']*3600;
 	}
